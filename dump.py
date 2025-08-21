4d23cd5da2331782b308fd472ee6efd980122d6c
ynchk1 <saspery@yandex.ru>
2025-08-21 17:29:40 +0300

    first commit

---
+++ dump.py
@@ -0,0 +1,41 @@
+import random
+import datetime
+import os 
+import click
+import time
+
+from vault_s3 import send_dump
+
+@click.group()
+def cli():
+    pass
+
+def gzip_file(path: str) -> int:
+    print(path)
+    code = os.system(f"gzip {path}")
+    return code
+
+def create_name() -> str:
+    r_int = random.randrange(0, 1000000000)
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime('%Y-%m-%d_%H:%M:%S')
+    name = f'{formatted_time}_{r_int}.tar'
+    return name
+
+@cli.command()
+@click.argument('database_name')
+@click.argument('path', default='/var/lib/postgresql/16/dumps/')
+def create_dump(database_name: str, path: str) -> None:
+    name = create_name()
+    full_path = f'{path}{name}'
+    exit_code = os.system(f"pg_dump -F tar -f '{full_path}' -U postgres {database_name}")
+
+    if exit_code != 0:
+       raise ValueError('Not a zero exit code')
+    if gzip_file(full_path) != 0:
+       raise ValueError('Not a zero exit code')
+    full_path = full_path + '.gz' 
+    send_dump(full_path, name)
+
+if __name__ == '__main__':
+    cli()
---
+++ vault_s3.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import boto3
+import os 
+
+load_dotenv()
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('SECRET_ACCESS_KEY'),
+    endpoint_url=os.getenv('S3_URL')
+)
+
+def send_dump(path_to_dump: str, name: str) -> None:
+    if os.path.isfile(path_to_dump):
+        try:
+            s3.upload_file(
+                path_to_dump,
+                os.getenv('BUCKET_NAME'),
+                f'onlineshop/{name}'
+            )
+        except Exception as e:
+            raise f'An exception occured: {e}'
+        print('Dumped successfuly')
+    else:
+        raise ValueError(f'An exception occured: file {path_to_dump} doesn\'t exists')
